32d4705e6ffab090a6e652198876d3550b6c5a9e
deep-sarkar <deepsarkar96@gmail.com>
2020-10-22 11:05:28 +0530

    [Deep]Add.added procedure to display all labels mapped with single note

--- sp_and_function/fundoo_sp.sql
+++ sp_and_function/fundoo_sp.sql
@@ -58,3 +58,34 @@ END$$
 
 
 
+
+-- CREATE procedure to give list of labels for a note
+CREATE PROCEDURE sp_get_labels(IN v_note_id int)
+BEGIN
+DECLARE v_label_name varchar(20);
+DECLARE v_label_list varchar(2000) default '';
+DECLARE v_finished integer default 0;
+DECLARE get_all_label_cursor CURSOR FOR SELECT name 
+        FROM label_label
+        INNER JOIN note_labelmap
+        ON label_label.id = note_labelmap.label_id
+        WHERE note_labelmap.note_id = v_note_id ;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_finished = 1;
+
+OPEN get_all_label_cursor;
+
+getLabels: LOOP
+    FETCH get_all_label_cursor INTO v_label_name;
+    if v_finished = 1
+    THEN leave getLabels;
+    END if;
+    set v_label_list = concat(v_label_list , concat(v_label_name, ',' ));
+END LOOP getLabels;
+CLOSE get_all_label_cursor;
+SELECT concat('[', concat(v_label_list, ']'));
+END$$
+
+    
+
+
+
