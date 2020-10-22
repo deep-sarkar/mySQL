2ed4f7c9c23465a50a55d01e61e80e761ec769cb
deep-sarkar <deepsarkar96@gmail.com>
2020-10-22 11:33:07 +0530

    [Deep]Add.added  function to get all collaborators for a perticular note

--- sp_and_function/fundoo_function.sql
+++ sp_and_function/fundoo_function.sql
@@ -24,3 +24,33 @@ END LOOP getLabels;
 CLOSE get_all_label_cursor;
 return concat('[', concat(v_label_list, ']'));
 END$$
+
+
+
+
+
+-- Create function to return all collaborator for a single note
+CREATE FUNCTION fn_get_collaborators(v_note_id integer)
+RETURNS varchar(2000) DETERMINISTIC
+BEGIN
+DECLARE v_collaborators varchar(2000) default '';
+DECLARE v_username varchar(20);
+DECLARE v_finished int default 0;
+DECLARE get_all_collab_cursor CURSOR FOR SELECT username 
+                                FROM auth_user
+                                INNER JOIN note_usermap
+                                ON auth_user.id = note_usermap.user_id
+                                WHERE note_usermap.note_id = v_note_id;
+DECLARE CONTINUE HANDLER FOR NOT FOUND set v_finished = 1;
+
+OPEN get_all_collab_cursor;
+getCollab: LOOP
+    FETCH get_all_collab_cursor INTO v_username;
+    if v_finished = 1
+    THEN leave getCollab;
+    END if;
+    set v_collaborators = concat(v_collaborators, concat(v_username, ','));
+END LOOP getCollab;
+CLOSE get_all_collab_cursor;
+return concat('[', concat(v_collaborators,']'));
+END$$
