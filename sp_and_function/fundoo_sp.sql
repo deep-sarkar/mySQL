d8028bbd4af4435ce636faf125c921e2e5cc4aa1
deep-sarkar <deepsarkar96@gmail.com>
2020-10-22 11:04:25 +0530

    [Deep]Add.added procedure to insert data into note_note table and show id

--- sp_and_function/fundoo_sp.sql
+++ sp_and_function/fundoo_sp.sql
@@ -42,4 +42,19 @@ SELECT user_id;
 END$$
 
 
+
+-- NOTE
 -- create note and return note id
+CREATE PROCEDURE sp_create_note_and_get_id(IN v_title varchar(100), IN v_note longtext, IN v_image varchar(100), 
+            IN v_reminder datetime(6), IN v_acrhive tinyint(1), IN v_trash tinyint(1), IN v_pin tinyint(1), IN v_color varchar(7),
+            IN v_user_id int)
+BEGIN
+INSERT INTO note_note(title , note, image, reminder, archive, trash, pin, color, user_id)
+    VALUES(v_title, v_note, v_image, v_reminder, v_acrhive, v_trash, v_pin, v_color, v_user_id);
+    SELECT LAST_INSERT_ID();
+END$$
+
+
+
+
+
